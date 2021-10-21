601e3bafdf34464f7302a96a445a164b283888c1
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-10-21 19:18:17 +0200

    repaired loading changes from the terminal

--- lib.py
+++ lib.py
@@ -1,17 +1,19 @@
 from pathlib import Path
 import csv
 
+
 class CSVReader:
 
     ALLOWED_EXTENSIONS = ('csv')
 
-    def __init__(self, path=None, destination=None, *changes):
+    def __init__(self, path, destination, changes):
         self.filename = Path(path).name
-        print(self.filename)
+        #print(self.filename)
         self.path = path
         self.parent_path = Path(self.path).parent  
         self.destination = destination
-        self.changes = list(changes)
+        self.changes = changes
+        #print(changes)
         self.cwd = Path.cwd()
         self.filetype = self.set_filetype()
         print(self.filetype)
@@ -27,20 +29,18 @@ class CSVReader:
                     print('Nieobsługiwany format.')
                     return 'wrong_ext'
                 else:
-                    print('val - valid_csv')
+                    #print('val - valid_csv')
                     return 'valid_csv'
             else:
-                print('val - valid_path')
-                #self.show_files(path)
+                #print('val - valid_path')
                 return 'valid_path'
         else:
-            print('val - invalid_path')
-            print(self.parent_path)
+            #print('val - invalid_path')
+            #print(self.parent_path)
             if Path(self.parent_path).is_dir():
                 return 'valid_parent_path'
             else:
                 return 'invalid_path'
-            #self.show_files(self.cwd)
 
     def set_filetype(self):
         print('*', Path(self.filename).suffix[1:])
@@ -50,21 +50,25 @@ class CSVReader:
 
     def show_files_in_dir(self):
         if self.validated == 'wrong_ext':
-            print('show - wrong_ext')
+            #print('show - wrong_ext')
             print(sorted(Path(self.parent_path).glob('*.csv')))
         elif self.validated == 'valid_path':
-            print('show - valid path')
+            #print('show - valid path')
             print(sorted(Path(self.path).glob('*.csv')))
         elif self.validated == 'valid_parent_path':
-            print('show - valid parent path')
+            #print('show - valid parent path')
             print(sorted(Path(self.parent_path).glob('*.csv')))
         else:
-            print('show - invalid_path')
+            #print('show - invalid_path')
             print(sorted(Path(self.cwd).glob('*.csv')))
 
     def validate_change(self):
-        pass
-            
-            
+        for element in self.changes:
+            splitted_element = element.split(',')
+            row_index = int(splitted_element[0])
+            column_index = int(splitted_element[1])
+            change_value = splitted_element[2]
+            print(row_index, column_index, change_value)        
+
     def mod_file(self):
         pass
--- reader.py
+++ reader.py
@@ -5,13 +5,14 @@ from lib import CSVReader
 source = sys.argv[1]
 destination = sys.argv[2]
 changes = sys.argv[3:]
+print(len(changes))
 
-'''changes "Y,X,wartosc" Y - wiersz(od 0), X - kolumna (od 0), wartosc - modyfikacja do wspisania w komórkę'''
+'''changes "Y,X,wartosc" Y - wiersz(od 0), X - kolumna (od 0), wartosc - modyfikacja do wpisania w komórkę'''
 
 reader = CSVReader(source, destination, changes)
 
 if reader.validated == 'valid_csv':
-    reader.mod_file()
+    reader.validate_change()
     
 else:
     reader.show_files_in_dir()
